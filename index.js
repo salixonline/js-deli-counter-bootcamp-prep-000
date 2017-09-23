f77d65f801d672cacd7b9ad0d3cacf025784436f
Armando <salixonline@gmail.com>
2017-09-23 07:40:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ function takeANumber(katzDeli, name) {
 	katzDeli.push(name);
 	return `Welcome, ${name}. You are number ${katzDeli.length} in line.`;
 }
+
+function nowServing(katzDeli) {
+	katzDeli.shift();
+	if (katzDeli.length === 0) {
+		return `There is nobody waiting to be served!`;
+	} else {
+		return `Currently serving ${katzDeli[0]}`;
+	}
+}
